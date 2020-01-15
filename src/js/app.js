9305ac414832b29346b0efa2a37f657007e72da7
rohanspencer <robo.spencer@gmail.com>
2020-01-15 16:24:44 +1100

    extended chartbuilder, added eslint, prior to moving to ractive

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,22 @@
-import loadJson from '../components/load-json/'
-import { Chartbuilder } from './modules/applet'
+import {
+  ChartBuilder
+} from "./modules/applet"
 
 
 function getURLParams(paramName) {
 
-	const params = window.parent.location.search.substring(1).split("&")
+  const params = window.parent.location.search.substring(1).split("&")
 
-    for (let i = 0; i < params.length; i++) {
-    	let val = params[i].split("=");
-	    if (val[0] == paramName) {
-	        return val[1];
-	    }
-	}
-	return null;
-
-}
-
-const key = (getURLParams('key')) ? getURLParams('key') : "1evrvPdWq4ysFW5JrdtJLDG3hVPNjPDnBqNJtT-WZnRo" ; 
-
-if ( key != null ) {
-
-	loadJson(`https://interactive.guim.co.uk/docsdata/${key}.json?t=${new Date().getTime()}`) 
-	      .then((data) => {
-	      	new Chartbuilder(data.sheets)
-	      })
+  for (let i = 0; i < params.length; i++) {
+    let val = params[i].split("=")
+    if (val[0] == paramName) {
+      return val[1]
+    }
+  }
+  return null
 
 }
 
+const key = (getURLParams("key")) ? getURLParams("key") : "1evrvPdWq4ysFW5JrdtJLDG3hVPNjPDnBqNJtT-WZnRo"
 
-
-
-
+new ChartBuilder(key)
