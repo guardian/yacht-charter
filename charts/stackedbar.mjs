d3e474326eda6be6ff28d1b0f5037f621d6a6979
Steven Tan <steven@steventan.com.au>
2020-12-04 17:12:29 +1100

    update stackedbar to use colour module

--- charts/stackedbar.mjs
+++ charts/stackedbar.mjs
@@ -2,10 +2,13 @@ import { numberFormat } from "../utilities/numberFormat"
 import mustache from "../utilities/mustache"
 import helpers from "../utilities/helpers"
 import Tooltip from "./shared/tooltip"
+import ColorScale from "./shared/colorscale"
 
 export default class StackedBarChart {
   constructor(results) {
     this.tooltip = new Tooltip("#graphicContainer")
+    this.colors = new ColorScale()
+    this.trendColors = new ColorScale()
     this.results = results
     this.render()
   }
@@ -18,18 +21,19 @@ export default class StackedBarChart {
     var labels = results.sheets.labels
     var trendline = results.sheets.trendline
     var userKey = results.sheets.key
-    var optionalKeys = []
-    var optionalColours = []
     var hasTooltip = details[0].tooltip != "" ? true : false
     var hasTrendline = trendline[0].index != "" ? true : false
     var template
+    var keys = Object.keys(data[0])
 
-    if (userKey.length > 1) {
-      userKey.forEach(function (d) {
-        optionalKeys.push(d.key)
-        optionalColours.push(d.colour)
-      })
-    }
+    const keyColor =
+      userKey.length > 1
+        ? this.colors.getUserDefinedKeysColors(userKey)
+        : {
+            keys
+          }
+
+    this.colors.set(keyColor.keys, keyColor.colors)
 
     if (hasTooltip) {
       template = details[0].tooltip
@@ -94,24 +98,7 @@ export default class StackedBarChart {
     var features = svg
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    var keys = Object.keys(data[0])
 
-    var colors = [
-      "#a6cee3",
-      "#1f78b4",
-      "#b2df8a",
-      "#33a02c",
-      "#fb9a99",
-      "#e31a1c",
-      "#fdbf6f",
-      "#ff7f00"
-    ]
-    var color = d3.scaleOrdinal()
-    if (userKey.length > 1) {
-      color.domain(optionalKeys).range(optionalColours)
-    } else {
-      color.domain(keys).range(colors)
-    }
     var xVar
     if (details[0]["xColumn"]) {
       xVar = details[0]["xColumn"]
@@ -121,13 +108,13 @@ export default class StackedBarChart {
       keys.splice(0, 1)
     }
 
-    keys.forEach(function (key, i) {
+    keys.forEach((key, i) => {
       var keyDiv = chartKey.append("div").attr("class", "keyDiv")
       keyDiv
         .append("span")
         .attr("class", "keyCircle")
-        .style("background-color", function () {
-          return color(key)
+        .style("background-color", () => {
+          return this.colors.get(key)
         })
       keyDiv.append("span").attr("class", "keyText").text(key)
     })
@@ -246,7 +233,7 @@ export default class StackedBarChart {
       .enter()
       .append("g")
       .attr("class", (d) => "layer " + d.key)
-      .style("fill", (d, i) => color(d.key))
+      .style("fill", (d, i) => this.colors.get(d.key))
     layer
       .selectAll("rect")
       .data((d) => d)
@@ -290,18 +277,10 @@ export default class StackedBarChart {
         }
       })
 
-      var trendColours = details[0].trendColours
-        ? details[0].trendColours.split(",")
-        : [
-            "#a6cee3",
-            "#1f78b4",
-            "#b2df8a",
-            "#33a02c",
-            "#fb9a99",
-            "#e31a1c",
-            "#fdbf6f",
-            "#ff7f00"
-          ]
+      if (details[0].trendColours) {
+        const tColors = details[0].trendColours.split(",")
+        this.trendColors.set(tColors.length, tColors)
+      }
 
       var colourIndex = 0
 
@@ -313,7 +292,7 @@ export default class StackedBarChart {
         features
           .append("path")
           .attr("d", tline(tdata))
-          .attr("stroke", trendColours[colourIndex])
+          .attr("stroke", this.trendColors.get(colourIndex))
           .attr("fill", "none")
 
         var keyDiv = chartKey.append("div").attr("class", "keyDiv")
@@ -321,7 +300,7 @@ export default class StackedBarChart {
         keyDiv
           .append("div")
           .attr("class", "keyDash")
-          .style("border-color", () => trendColours[colourIndex])
+          .style("border-color", () => this.trendColors.get(colourIndex))
 
         keyDiv.append("span").attr("class", "keyText").text(tkeys[colourIndex])
 
