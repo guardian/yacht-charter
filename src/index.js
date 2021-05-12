0301715a1241c7cd4c5d2c4a8d8b07792e61ad26
jerseywonder <work@andyball.info>
2021-05-12 10:05:37 +1000

    Fix issue with axis lable on grouped bar

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import {
 } from "./modules/chart"
 import "./main.scss"
 
-const key = getURLParams("key") ? getURLParams("key") : "12uVYXiRLojmJXPXSGZ8BqtcHVG0IvtzI6Q_Xrxq2lyY"
-const location = getURLParams("location") ? getURLParams("location") : "docsdata"
+const key = getURLParams("key") ? getURLParams("key") : "victransport_patronage_covid"
+const location = getURLParams("location") ? getURLParams("location") : "yacht-charter-data"
 
 new Chart(key, location)
+
+
+// https://interactive.guim.co.uk/embed/aus/2020/yacht-charter-v15/index.html?key=oz_vaccine_tracker_goals_trend_two_trend_test&location=yacht-charter-data
