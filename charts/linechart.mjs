148028924fbb9614d2a2c88e6aba583ab8f9a421
jerseywonder <work@andyball.info>
2021-08-31 12:18:15 +1000

    Move linechart sonification stuff to its own module

--- charts/linechart.mjs
+++ charts/linechart.mjs
@@ -6,12 +6,10 @@ import Tooltip from "./shared/tooltip"
 import ColorScale from "./shared/colorscale"
 import renderCanvas from "../utilities/renderCanvas"
 import dimensions from "./constants/dimensions"
-import * as tone from 'tone'
-const timer = ms => new Promise(res => setTimeout(res, ms)) 
-import createElement from "./shared/createElement"
 import templatizer from "./shared/templatizer"
 import template from "./templates/linechart"
-//import sonic from "./sonic"
+import sonic from "./sonic"
+const Sonic =  new sonic()
 
 
 /****** Example tooltip template */
@@ -669,9 +667,7 @@ export default class LineChart {
 
 		if (this.options.aria==='TRUE') {
 
-			this.sonic()
-
-			//sonic(this.sonicData, this.x, this.y, this.keyOrder, this.margin, this.y.domain(), 'svg')
+			Sonic.ification(this.sonicData, this.x, this.y, this.keyOrder, this.margin, this.y.domain(), 'svg')
 
 		}
 
@@ -891,213 +887,4 @@ export default class LineChart {
 
 	}
 
-	sonic() {
-
-		var self = this
-
-		const bpm = 400
-
-		const note = 60 / bpm
-
-		const low = 130.81
-
-		const high = 261.63
-
-		const domain = this.y.domain()
-
-		const scale = d3.scaleLinear()
-		      .domain(domain)
-		      .range([low,high])
-
-		var button = createElement("button",{"id":"sonic", "tabindex" : "1", "tabindex" : "1", "aria-label" : "click to hear the chart values", "role" : "button"})
-
-		var furniture = document.querySelector('#furniture');
-
-		furniture.appendChild(button);
-
-		var sonicButton = document.querySelector('#sonic');
-		sonicButton.addEventListener('click', noiseLoop);
-		sonicButton.addEventListener('keydown', sonicButtonKeydownHandler);
-		sonicButton.addEventListener('keyup', sonicButtonKeyupHandler);
-
-		function sonicButtonKeydownHandler (event) {
-		  // The action button is activated by space on the keyup event, but the
-		  // default action for space is already triggered on keydown. It needs to be
-		  // prevented to stop scrolling the page before activating the button.
-		  if (event.keyCode === 32) {
-		    event.preventDefault();
-		  }
-		  // If enter is pressed, activate the button
-		  else if (event.keyCode === 13) {
-		    event.preventDefault();
-		    noiseLoop();
-		  }
-		}
-
-		function sonicButtonKeyupHandler (event) {
-		  if (event.keyCode === 32) {
-		    event.preventDefault();
-		    noiseLoop();
-		  }
-		}
-
-		this.$svg.append("circle")
-				.attr("r",5)
-				.attr("stroke", "red")
-				.attr("cx",self.x(self.sonicData[self.keyOrder[0]][0]['Date']) + self.margin.left)
-				.attr("cy",self.y(self.sonicData[self.keyOrder[0]][0][self.keyOrder[0]]) + self.margin.top)
-				.attr("fill","none")
-				.attr("id", "playHead")
-		      
-		function makeNoise(xVar, yVar) {
-		    
-		    var synth = new tone.Synth({
-		      envelope: {
-		        decay: 0,
-		        sustain:1,
-		        release:0.5
-		      },
-		      oscillator : {
-		        count: 8,
-		        spread: 30,
-		        type : "sawtooth4"
-		      }
-		    }
-		    ).toDestination();
-
-		    self.sonicData[yVar].forEach(function(d,i) {		    	
-
-		      if (i == 0) { 
-		        synth.triggerAttackRelease(scale(d[yVar]), self.sonicData[yVar].length * note).onsilence(clearSynth())
-		        d3.select("#playHead")
-		            .attr("cx",self.x(self.sonicData[yVar][i][xVar]) + self.margin.left)
-		            .attr("cy",self.y(self.sonicData[yVar][i][yVar]) + self.margin.top)
-		      }
-		      else {
-		        tone.Transport.schedule(function(){
-		          d3.select("#playHead").transition().duration(500)
-		            .ease(d3.easeLinear)
-		            .attr("cx",self.x(self.sonicData[yVar][i][xVar]) + self.margin.left)
-		            .attr("cy",self.y(self.sonicData[yVar][i][yVar]) + self.margin.top)
-		          
-		          synth.frequency.rampTo(scale(d[yVar]), note);
-		        }, i * note);
-		      }
-
-		    })
-		  
-		    tone.Transport.position = "0:0:0"
-		    tone.Transport.start()
-		  
-		    function clearSynth () {
-		      console.log("finished")
-		    }
-
-		}
-
-		function beep(key) {
-
-		    return new Promise( (resolve, reject) => {
-
-			    var synth = new tone.Synth({
-			      envelope: {
-			        decay: 0,
-			        sustain:1,
-			        release:0.5
-			      },
-			      oscillator : {
-			        count: 8,
-			        spread: 30,
-			        type : "sawtooth4"
-			      }
-			    }
-			    ).toDestination();
-
-			    synth.triggerAttackRelease(key, 1).onend(clearSynth())
-
-			   	tone.Transport.position = "0:0:0"
-
-			    tone.Transport.start()
-
-			    function clearSynth () {
-			      resolve({ status : "success"})
-			    }
-
-
-		    }).catch(function(e) {
-
-			  reject(e);
-
-			});
-		  
-		}
-
-		function speaker(text) {
-
-		    return new Promise( (resolve, reject) => {
-
-				if ('speechSynthesis' in window) {
-				 
-				 	var msg = new SpeechSynthesisUtterance();
-
-				 	msg.text = text
-
-				 	window.speechSynthesis.speak(msg);
-
-				 	msg.onend = function() {
-
-				 		resolve({ status : "success"})
-
-				 	};
-
-				} else {
-
-					resolve({ status : "no txt to speach"})
-
-				}
-
-		    }).catch(function(e) {
-
-			  reject(e);
-
-			});
-		}
-
-		async function noiseLoop() {
-
-			if (!this.isPlaying) {
-
-				this.isPlaying = true
-
-				const text1 = await speaker(`The lowest value on the chart is ${domain[0]}, and it sounds like `)
-
-				const beep1 = await beep(scale(domain[0]))
-
-				await timer(1200);
-
-				const text2 = await speaker(`The highest value on the chart is ${domain[1]}, and it sounds like `)
-
-				const beep2 = await beep(scale(domain[1]))
-
-				await timer(1200);
-
-				for await (const datastream of self.keyOrder) {
-
-			        d3.select("#playHead")
-		            .attr("cx",self.x(self.sonicData[datastream][0]['Date']) + self.margin.left)
-		            .attr("cy",self.y(self.sonicData[datastream][0][datastream]) + self.margin.top)
-
-					const category = await speaker(datastream)
-
-					makeNoise('Date', datastream)
-
-					await timer(self.sonicData[datastream].length * note * 1000);
-
-				}
-
-				this.isPlaying = false
-
-			}
-		}
-	}
 }
--- charts/sonic/index.mjs
+++ charts/sonic/index.mjs
@@ -1,226 +1,28 @@
-import * as tone from 'tone'
-const timer = ms => new Promise(res => setTimeout(res, ms)) 
 
-// export default function sonic(sonicData, x, y, keyOrder, margin, domain, svg) {
+import sonification from "./sonification"
 
-var sonic = {
+export default class sonic {
 
-  init: function(sonicData, x, y, keyOrder, margin, domain, svg) {
+  constructor() {
 
-    const bpm = 400
+    this.isInitiated = false
 
-    const note = 60 / bpm
+  }
 
-    const low = 130.81
+  ification(sonicData, x, y, keyOrder, margin, domain, svg) {
 
-    const high = 261.63
+    if (!this.isInitiated) {
 
-    let isPlaying = false
+      this.isInitiated = true
 
-    const scale = d3.scaleLinear()
-          .domain(domain)
-          .range([low,high])
+      sonification.init(sonicData, x, y, keyOrder, margin, domain, svg)
 
-    var button = document.createElement("button")
+    } else {
 
-    button.setAttribute("id", "sonic");
-
-    button.setAttribute("tabindex", "1");
-
-    button.setAttribute("aria-label", "click to hear the chart values");
-
-    button.setAttribute("role", "button");
-
-    var furniture = document.getElementById('furniture');
-
-    furniture.appendChild(button);
-
-    var sonicButton = document.getElementById('sonic');
-    sonicButton.addEventListener('click', noiseLoop);
-    sonicButton.addEventListener('keydown', sonicButtonKeydownHandler);
-    sonicButton.addEventListener('keyup', sonicButtonKeyupHandler);
-
-    d3.select(`#${svg}`).append("circle")
-        .attr("r",5)
-        .attr("stroke", "red")
-        .attr("cx",x(sonicData[keyOrder[0]][0]['Date']) + margin.left)
-        .attr("cy",y(sonicData[keyOrder[0]][0][keyOrder[0]]) + margin.top)
-        .attr("fill","none")
-        .attr("id", "playHead")
-
-  },
-
-  sonicButtonKeydownHandler: function(event) {
-
-    if (event.keyCode === 32) {
-      event.preventDefault();
-    }
-    // If enter is pressed, activate the button
-    else if (event.keyCode === 13) {
-      event.preventDefault();
-      noiseLoop();
-    }
-  },
-
-  sonicButtonKeyupHandler: function(event) {
-    if (event.keyCode === 32) {
-      event.preventDefault();
-      noiseLoop();
-    }
-  },
-        
-  makeNoise: function(xVar, yVar) {
-      
-      var synth = new tone.Synth({
-        envelope: {
-          decay: 0,
-          sustain:1,
-          release:0.5
-        },
-        oscillator : {
-          count: 8,
-          spread: 30,
-          type : "sawtooth4"
-        }
-      }
-      ).toDestination();
-
-      sonicData[yVar].forEach(function(d,i) {          
-
-        if (i == 0) { 
-          synth.triggerAttackRelease(scale(d[yVar]), sonicData[yVar].length * note).onsilence(clearSynth())
-          d3.select("#playHead")
-              .attr("cx",x(sonicData[yVar][i][xVar]) + margin.left)
-              .attr("cy",y(sonicData[yVar][i][yVar]) + margin.top)
-        }
-        else {
-          tone.Transport.schedule(function(){
-            d3.select("#playHead").transition().duration(500)
-              .ease(d3.easeLinear)
-              .attr("cx",x(sonicData[yVar][i][xVar]) + margin.left)
-              .attr("cy",y(sonicData[yVar][i][yVar]) + margin.top)
-            
-            synth.frequency.rampTo(scale(d[yVar]), note);
-          }, i * note);
-        }
-
-      })
-    
-      tone.Transport.position = "0:0:0"
-      tone.Transport.start()
-    
-      function clearSynth () {
-        console.log("finished")
-      }
-
-  },
-
-  beep : function(key) {
-
-      return new Promise( (resolve, reject) => {
-
-        var synth = new tone.Synth({
-          envelope: {
-            decay: 0,
-            sustain:1,
-            release:0.5
-          },
-          oscillator : {
-            count: 8,
-            spread: 30,
-            type : "sawtooth4"
-          }
-        }
-        ).toDestination();
-
-        synth.triggerAttackRelease(key, 1).onend(clearSynth())
-
-        tone.Transport.position = "0:0:0"
-
-        tone.Transport.start()
-
-        function clearSynth () {
-          resolve({ status : "success"})
-        }
-
-
-      }).catch(function(e) {
-
-      reject(e);
-
-    });
-    
-  },
-
-  speaker: function(text) {
-
-      return new Promise( (resolve, reject) => {
-
-      if ('speechSynthesis' in window) {
-       
-        var msg = new SpeechSynthesisUtterance();
-
-        msg.text = text
-
-        window.speechSynthesis.speak(msg);
-
-        msg.onend = function() {
-
-          resolve({ status : "success"})
-
-        };
-
-      } else {
-
-        resolve({ status : "no txt to speach"})
-
-      }
-
-      }).catch(function(e) {
-
-      reject(e);
-
-    });
-  },
-
-  noiseLoop: async function() {
-
-    if (!isPlaying) {
-
-      isPlaying = true
-
-      const text1 = await speaker(`The lowest value on the chart is ${domain[0]}, and it sounds like `)
-
-      const beep1 = await beep(scale(domain[0]))
-
-      await timer(1200);
-
-      const text2 = await speaker(`The highest value on the chart is ${domain[1]}, and it sounds like `)
-
-      const beep2 = await beep(scale(domain[1]))
-
-      await timer(1200);
-
-      for await (const datastream of keyOrder) {
-
-            d3.select("#playHead")
-              .attr("cx",x(sonicData[datastream][0]['Date']) + margin.left)
-              .attr("cy",y(sonicData[datastream][0][datastream]) + margin.top)
-
-        const category = await speaker(datastream)
-
-        makeNoise('Date', datastream)
-
-        await timer(sonicData[datastream].length * note * 1000);
-
-      }
-
-      isPlaying = false
+      sonification.update(x, y)
 
     }
 
   }
 
 }
-
-export default sonic
